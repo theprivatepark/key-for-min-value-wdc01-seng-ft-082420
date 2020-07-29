722497e8b60dbc66d110e7d4226bf20421ee6b4c
Anney Park <anneypark@gmail.com>
2020-07-29 16:20:35 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@
 def key_for_min_value(name_hash)
  smallest_value = name_hash.values[0]
  current_smallest_key = name_hash.key[0]
+ 
  name_hash.each do |name, new_value|
    if smallest_value > new_value    #500 > 2
      smallest_value = new_value     #smallest_value = 1
