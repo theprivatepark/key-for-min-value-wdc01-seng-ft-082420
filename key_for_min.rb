562cbe0b3d1a410f7ad0754d919b24e55bc54d68
Anney Park <anneypark@gmail.com>
2020-07-29 14:43:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 # name_hash = {:blake => 500, :ashley => 2, adam => 1}
 
-def key_for_min_value(name_hash) #defining method
-  name_hash.each do |name,value| #enumberable .each - loops
+def key_for_min_value(name_hash)          #defining method
+  name_hash.each do |key,value|           #enumberable .each - loops
   
-    current_small = #{name},#{value}
+    current_small = #{key},#{value}       #blake,500
     
-    new_smallest = value[0]
     
     count = 0
-    if current_small < value[0] count #comparing current small w new small
+    if current_small < value[0].count #comparing current small w new small
       new_smallest = value[0]
     end
     smallest_value
