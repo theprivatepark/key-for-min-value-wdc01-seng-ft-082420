36c8bc40017f43b1626b5240817e9a32cc52fac2
Anney Park <anneypark@gmail.com>
2020-07-29 14:03:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,8 @@
 
 def key_for_min_value(name_hash) #defining method
   name_hash.each do |name,value| #enumberable each
-    smallest_value = value 
-    smallest_name = name
+    current_smallest_value = value
+  
     if value < smallest_value #comparing current value with smallest_value
       smallest_value = value
       smallest_name = name
