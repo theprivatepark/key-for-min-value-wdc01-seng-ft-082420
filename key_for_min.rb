59a1c2c61ad2adeeac56871606fdc8a14774c6e1
Anney Park <anneypark@gmail.com>
2020-07-29 14:58:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,6 +11,7 @@ def key_for_min_value(name_hash)          #defining method
   
     if current_small < value       #comparing current small w new small
       new_smallest = value
+      
       smallest_value = key
       
     end
